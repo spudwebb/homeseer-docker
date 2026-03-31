688c910efadb5b5f26d92e51fb54b1786568d2b7
spud <spud.webb.4@gmail.com>
2026-03-31 17:18:42 -0400

    Update build script for new HS release version 4.2.23.0

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ build () {
 }
 
 # latest beta build
-build "4.2.22.99" "https://homeseer.com/updates4/linux_4_2_22_99.tar.gz" "--tag spudwebb/homeseer:beta" "${EXTRA_ARGS[@]}"
+#build "4.2.22.99" "https://homeseer.com/updates4/linux_4_2_22_99.tar.gz" "--tag spudwebb/homeseer:beta" "${EXTRA_ARGS[@]}"
 
 # latest release build
-build "4.2.22.4" "https://homeseer.com/updates4/linux_4_2_22_4.tar.gz" "--tag spudwebb/homeseer:latest" "${EXTRA_ARGS[@]}"
+build "4.2.23.0" "https://homeseer.com/updates4/linux_4_2_23_0.tar.gz" "--tag spudwebb/homeseer:latest" "${EXTRA_ARGS[@]}"
