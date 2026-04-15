1c7ed205956c09f827e936b2ab9680cd9eb5a583
spud <spud.webb.4@gmail.com>
2026-04-15 08:17:43 -0400

    Use debian:trixie-slim as base and install mono on it, instead of using mono:6.12.0 which was based on debian:buster-slim

--- base/build.sh
+++ base/build.sh
@@ -6,7 +6,7 @@
 
 
 # docker image version
-VERSION="2.0"
+VERSION="3.0"
 
 echo
 echo "**********************************************************************"
@@ -23,7 +23,8 @@ docker buildx create --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=10485760   \
 docker buildx build \
   --build-arg BUILDDATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
   --build-arg VERSION="$VERSION" \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  #--platform linux/amd64,linux/arm64,linux/arm/v7 
+  --platform linux/amd64 \
   --push \
   --tag spudwebb/homeseer-base:$VERSION \
   --tag spudwebb/homeseer-base:latest \
